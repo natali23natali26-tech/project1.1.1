8456ce4258040970dd4117c90af8f42007d56b67
Natalia Gavrilova <nattylek68@mail.ru>
2025-09-01 11:46:19 +0300

    processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,3 +1,5 @@
+import datetime
+
 def filter_by_state(data, state='EXECUTED'):
     """
     Фильтрует список словарей, оставляя только те, у которых значение ключа 'state'
@@ -16,6 +18,21 @@ def filter_by_state(data, state='EXECUTED'):
     return filtered_data
 
 
+def sort_by_date(data, reverse=True):
+    """
+    Сортирует список словарей по дате.
+
+    Args:
+        data (list): Список словарей.
+        reverse (bool, optional): Направление сортировки (True - убывание, False - возрастание).
+            По умолчанию True (убывание).
+
+    Returns:
+        list: Новый список, отсортированный по дате.
+    """
+    return sorted(data, key=lambda x: datetime.datetime.fromisoformat(x['date']), reverse=reverse)
+
+
 if __name__ == '__main__':
     data = [
         {'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
@@ -24,10 +41,20 @@ if __name__ == '__main__':
         {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}
     ]
 
+    # Пример работы filter_by_state
     executed_data = filter_by_state(data)
-    print("Выход функции со статусом по умолчанию 'EXECUTED':")
+    print("Выход функции filter_by_state со статусом по умолчанию 'EXECUTED':")
     print(executed_data)
 
     canceled_data = filter_by_state(data, 'CANCELED')
-    print("\nВыход функции, если вторым аргументов передано 'CANCELED':")
+    print("\nВыход функции filter_by_state, если вторым аргументом передано 'CANCELED':")
     print(canceled_data)
+
+    # Пример работы sort_by_date
+    sorted_data_desc = sort_by_date(data)
+    print("\nВыход функции sort_by_date (сортировка по убыванию):")
+    print(sorted_data_desc)
+
+    sorted_data_asc = sort_by_date(data, reverse=False)
+    print("\nВыход функции sort_by_date (сортировка по возрастанию):")
+    print(sorted_data_asc)

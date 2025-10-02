53f0d3a8d284c847af656aaca768e1b230631fe8
Natalie Gavrilova <natali23natali26-tech@users.noreply.github.com>
2025-10-02 21:52:17 +0300

    correct test external_api

--- tests/test_external_api.py
+++ tests/test_external_api.py
@@ -1,5 +1,7 @@
 import pytest
-from unittest.mock import patch, Mock
+from unittest.mock import patch
+
+
 from src.external_api import get_exchange_rate
 
 @pytest.fixture
@@ -7,21 +9,14 @@ def mock_env():
     with patch('os.getenv', return_value='mock_api_key'):
         yield
 
-def test_get_exchange_rate_success(mock_env):
-    with patch('requests.get') as mock_get:
-        # Настройка мока для ответа от requests.get
-        mock_response = Mock()
-        mock_response.json.return_value = {
-            'rates': {
-                'USD': 70.5
-            }
-        }
-        mock_response.raise_for_status = Mock()
-        mock_get.return_value = mock_response
 
-        # Вызов функции и проверка результата
-        rate = get_exchange_rate('USD')
-        assert rate == 70.5
+@patch('requests.get')
+def test_get_exchange_rate_success(mock_get):
+    mock_get.return_value.json.return_value = {'rates': {'RUB': 70.5 }}
+    mock_get.return_value.status_code = 200
+    rate = get_exchange_rate('USD')
+    assert rate == 70.5
+
 
 def test_get_exchange_rate_failure(mock_env):
     with patch('requests.get') as mock_get:

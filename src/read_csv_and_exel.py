f16411cef1ccf023d6232b03ffe73407cf613211
Natalie Gavrilova <natali23natali26-tech@users.noreply.github.com>
2025-10-09 23:12:44 +0300

    read_csv_and_exel docstring

--- src/read_csv_and_exel.py
+++ src/read_csv_and_exel.py
@@ -12,14 +12,14 @@ def read_transactions_from_csv(csv_path):
         list: Список словарей с транзакциями.
     """
     try:
-        df = pd.read_csv(csv_path)
-        transactions = df.to_dict(orient='records')
+        df = pd.read_csv(csv_path)  # Читаем данные из CSV файла в DataFrame
+        transactions = df.to_dict(orient='records')  # Преобразуем DataFrame в список словарей
         return transactions
     except FileNotFoundError:
-        print(f"Ошибка: Файл не найден по пути {csv_path}")
+        print(f"Ошибка: Файл не найден по пути {csv_path}")  # Обрабатываем ошибку, если файл не найден
         return []
     except Exception as e:
-        print(f"Ошибка при чтении CSV-файла: {e}")
+        print(f"Ошибка при чтении CSV-файла: {e}")  # Обрабатываем любые другие исключения
         return []
 
 
@@ -34,23 +34,25 @@ def read_transactions_from_excel(excel_path):
         list: Список словарей с транзакциями.
     """
     try:
-        df = pd.read_excel(excel_path)
-        transactions = df.to_dict(orient='records')
-        return transactions
+        df = pd.read_excel(excel_path)  # Читаем данные из Excel файла в DataFrame
+        transactions = df.to_dict(orient='records')  # Преобразуем DataFrame в список словарей
+        return transactions  # Возвращаем список транзакций
     except FileNotFoundError:
-        print(f"Ошибка: Файл не найден по пути {excel_path}")
+        print(f"Ошибка: Файл не найден по пути {excel_path}")  # Обрабатываем ошибку, если файл не найден
         return []
     except Exception as e:
-        print(f"Ошибка при чтении Excel-файла: {e}")
+        print(f"Ошибка при чтении Excel-файла: {e}")  # Обрабатываем любые другие исключения
         return []
 
 
 if __name__ == '__main__':
+    # Указываем пути к файлам CSV и Excel
     csv_file_path = r"C:\Users\nata6\PycharmProjects\my_prg\Client's personal account\data\transactions.csv"
     excel_file_path = r"C:\Users\nata6\PycharmProjects\my_prg\Client's personal account\data\transactions_excel.xlsx"
 
-    csv_transactions = read_transactions_from_csv(csv_file_path)
-    excel_transactions = read_transactions_from_excel(excel_file_path)
+    # Читаем транзакции из файлов
+    csv_transactions = read_transactions_from_csv(csv_file_path)  # Чтение из CSV
+    excel_transactions = read_transactions_from_excel(excel_file_path)  # Чтение из Excel
 
     print("Транзакции из CSV:")
     for transaction in csv_transactions:
